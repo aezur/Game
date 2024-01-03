a3c92577451cd9978bdbd7175d7cc26e05f57632
Peter Mulligan <aezur20@gmail.com>
2024-01-03 02:12:42 +0100

    Added frontend types.

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,12 +1,28 @@
 export interface User {
-    id: number;
-    name: string;
-    email: string;
-    email_verified_at: string;
+  id: number;
+  name: string;
+  email: string;
+  email_verified_at: string;
+}
+
+export interface Ludus {
+  id: number;
+  owner: number;
+  name: string;
+}
+
+export interface Gladiator {
+  id: number;
+  name: string;
+  strength: number;
+  defense: number;
+  accuracy: number;
+  evasion: number;
+  ludus: number | null;
 }
 
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
-    auth: {
-        user: User;
-    };
+  auth: {
+    user: User;
+  };
 };
