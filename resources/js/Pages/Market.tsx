2e9e0be13d636dfd71d605ba876156612681136f
Peter Mulligan <aezur20@gmail.com>
2024-01-08 09:33:56 +0100

    Made responsive, and added modal guard before purchase.

--- resources/js/Pages/Market.tsx
+++ resources/js/Pages/Market.tsx
@@ -2,11 +2,14 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 import { MarketGladiator, PageProps } from "@/types";
 import PrimaryButton from "@/Components/PrimaryButton";
-import { useEffect, useMemo, useState } from "react";
+import { Fragment, useEffect, useMemo, useState } from "react";
 import { useAxios } from "@/Hooks/useAxios";
 import { useErrorHandler } from "@/Hooks/useErrorHandler";
 import { router } from "@inertiajs/core";
 import Card from "@/Components/Card";
+import Modal from "@/Components/Modal";
+import SecondaryButton from "@/Components/SecondaryButton";
+import { useMidnightCountdown } from "@/Hooks/useMidnightCountdown";
 
 export default function Market({
   auth,
@@ -16,10 +19,24 @@ export default function Market({
   const [isSubmitting, setIsSubmitting] = useState(false);
   const { axios } = useAxios();
   const { handleError } = useErrorHandler();
+  const [showModal, setShowModal] = useState(false);
+  const [selectedGladiator, setSelectedGladiator] = useState<MarketGladiator>();
+  const { formattedCounter } = useMidnightCountdown();
 
   const leftToPurchase = useMemo(() => {
     return gladiators.filter((g) => !g.purchased).length;
   }, [gladiators]);
+
+  const openModal = (gladiator: MarketGladiator) => {
+    setSelectedGladiator(gladiator);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    // Wait for the modal to close before resetting the selected gladiator
+    setSelectedGladiator(undefined);
+  };
 
   const purchase = async (id: number) => {
     try {
@@ -36,44 +53,23 @@ export default function Market({
     } catch (error) {
       handleError(error);
     } finally {
+      closeModal();
       setIsSubmitting(false);
     }
   };
 
-  function Countdown() {
-    const [counter, setCounter] = useState(
-      // Seconds to midnight
-      24 * 60 * 60 -
-        new Date().getHours() * 60 * 60 -
-        new Date().getMinutes() * 60 -
-        new Date().getSeconds()
-    );
-
-    useEffect(() => {
-      // If the counter is 0, reload the page to refresh the gladiator list
-      if (counter <= 0) {
-        router.reload({ preserveScroll: true });
-      }
-      const timer =
-        counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
-      return () => {
-        if (timer) clearInterval(timer);
-      };
-    }, [counter]);
-
-    const formattedCounter = useMemo(() => {
-      const hours = Math.floor(counter / 3600);
-      const minutes = Math.floor((counter - hours * 3600) / 60);
-      const seconds = counter - hours * 3600 - minutes * 60;
-      return `${hours.toString().padStart(2, "0")}:${minutes
-        .toString()
-        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-    }, [counter]);
-
+  function Countdown({
+    text = "Refreshes in ",
+    className = "",
+  }: {
+    text?: string;
+    className?: string;
+  }) {
     return (
-      <p>
-        Refreshes in <pre className="inline">{formattedCounter}</pre>
-      </p>
+      <div className={`flex ${className}`}>
+        <p className="mr-2">{text}</p>
+        <pre className="inline">{formattedCounter}</pre>
+      </div>
     );
   }
 
@@ -82,20 +78,25 @@ export default function Market({
       <p className="mb-4">
         These men have never stepped foot on the battlefield, let alone in the
         arena. While they might be untested and untrained, they are also cheap
-        and easily replaceable. Most are slaves, criminals, or prisoners of war,
-        but that doesn't mean they can't be trained into a formidable gladiator.
+        and easily replaceable. Most are slaves, criminals, or brigands, but
+        that doesn't mean they can't be trained into a formidable gladiator.
       </p>
     );
   }
 
   function TableHeader() {
     return (
-      <div className="py-2 text-lg grid grid-cols-8">
-        <p>Name</p>
-        <p className="text-center truncate text-ellipsis">Strength</p>
-        <p className="text-center truncate text-ellipsis">Accuracy</p>
-        <p className="text-center truncate text-ellipsis">Defense</p>
-        <p className="text-center truncate text-ellipsis">Evasion</p>
+      <div className="py-2 text-lg sm:grid grid-cols-8 hidden">
+        {Object.keys(gladiators[0])
+          .filter((key) => key !== "id")
+          .map((key) => (
+            <h4
+              key={key}
+              className="capitalize text-center first-of-type:text-left"
+            >
+              {key}
+            </h4>
+          ))}
       </div>
     );
   }
@@ -110,18 +111,22 @@ export default function Market({
 
   function GladiatorDisplay({ gladiator }: { gladiator: MarketGladiator }) {
     return (
-      <div className="py-2 text-2xl grid grid-cols-8 border-b border-gray-600">
-        <p>{gladiator.name}</p>
-        <p className="text-center">{gladiator.strength}</p>
-        <p className="text-center">{gladiator.accuracy}</p>
-        <p className="text-center">{gladiator.defense}</p>
-        <p className="text-center">{gladiator.evasion}</p>
-        <p className="text-right">{gladiator.price} 𐆖</p>
+      <div className="py-2 text-2xl grid sm:grid-cols-8 border-b border-gray-600 grid-cols-2">
+        {Object.entries(gladiator)
+          .filter(([key, value]) => key !== "id")
+          .map(([key, value]) => (
+            <Fragment key={key}>
+              <h4 className="capitalize sm:hidden visible">{key}</h4>
+              <p className="sm:first-of-type:text-left text-ellipsis truncate text-center">
+                {`${value} ${key === "price" ? "𐆖" : ""}`}
+              </p>
+            </Fragment>
+          ))}
         <div className="col-span-2 mx-auto flex items-center">
           <PrimaryButton
-            className="py-0"
+            className="py-0 mb-4 sm:mb-0"
             disabled={isSubmitting}
-            onClick={() => purchase(gladiator.id)}
+            onClick={() => openModal(gladiator)}
           >
             Buy
           </PrimaryButton>
@@ -147,17 +152,41 @@ export default function Market({
           <Card className="mb-8">
             <h3 className="font-semibold text-xl leading-tight">The Dregs</h3>
             <FlavorText />
-            {leftToPurchase > 0 && <TableHeader />}
-            <div>
-              {gladiators?.map((g: MarketGladiator) => {
-                return g.purchased ? (
-                  <PurchasedDisplay key={g.id} />
-                ) : (
-                  <GladiatorDisplay key={g.id} gladiator={g} />
-                );
-              })}
-            </div>
+            {leftToPurchase > 0 ? (
+              <>
+                <TableHeader />
+                {gladiators?.map((g: MarketGladiator) => {
+                  return g.purchased ? (
+                    <PurchasedDisplay key={g.id} />
+                  ) : (
+                    <GladiatorDisplay key={g.id} gladiator={g} />
+                  );
+                })}
+              </>
+            ) : (
+              <>
+                <p className="text-center text-xl">
+                  There are no more gladiators to purchase today.
+                </p>
+                <div className="flex justify-center text-2xl">
+                  <Countdown className="text-center" text="Come back in " />
+                </div>
+              </>
+            )}
           </Card>
+          <Modal show={showModal} onClose={closeModal}>
+            {selectedGladiator && (
+              <div className="m-4 text-3xl text-gray-800 dark:text-gray-200 text-center">
+                <p className="mb-4">{`Purchase ${selectedGladiator.name} for ${selectedGladiator.price} 𐆖?`}</p>
+                <div className="flex justify-center items-center gap-4">
+                  <SecondaryButton onClick={closeModal}>Cancel</SecondaryButton>
+                  <PrimaryButton onClick={() => purchase(selectedGladiator.id)}>
+                    Purchase
+                  </PrimaryButton>
+                </div>
+              </div>
+            )}
+          </Modal>
         </div>
       </div>
     </AuthenticatedLayout>
